27ec8a37c4e8fce14b4b5a79d51e441a8789efc5
shashank-16 <shashank.chaurasia1606@gmail.com>
2023-05-15 15:30:36 +0530

    some paremeter to set like access of the database from public

--- project/config/SetUp.php
+++ project/config/SetUp.php
@@ -30,10 +30,17 @@ if($connection)
         create table exam_record (S_no int(11) PRIMARY KEY AUTO_INCREMENT , user_name varchar(100));
         create table subject_info( subject varchar(20) , no_question int(11) , duration int(10));
         create table teacher_info( Institute_name text , branch text,teacher_name varchar(100),Username varchar(50) , Password varchar(100),SubjectCode varchar(10));
-        create table user_info( student_id int(6),branch text,name_student varchar(30),user_name varchar(50),password varchar(20) );
+        create table user_info( student_Id int(6),branch text,name_student varchar(30),user_name varchar(50),password varchar(20) );
         ";
 
-        $mysqli_automatic = mysqli_multi_query($main_connection,$automation_setup) or die("error occur during setup".mysqli_connect_errno());
+        $mysqli_automatic = mysqli_multi_query($main_connection,$automation_setup);
+        if($mysqli_automatic)
+        {
+            echo("database has been set");
+        }   
+        else{
+            die("error occur during setup".mysqli_connect_errno());
+        }
     }
 
 }
